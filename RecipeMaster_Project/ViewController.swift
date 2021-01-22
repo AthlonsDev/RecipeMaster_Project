19fe0398f523f7adfefc355c2d429079fd2bf663
Andrea Sanna <athlons91@gmail.com>
2021-01-22 10:57:19 +0000

    Initial Commit

---
+++ RecipeMaster_Project/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RecipeMaster_Project
+//
+//  Created by Athlosn90 on 22/01/2021.
+//  Copyright © 2021 AndreaSanna. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ RecipeMaster_ProjectTests/RecipeMaster_ProjectTests.swift
@@ -0,0 +1,34 @@
+//
+//  RecipeMaster_ProjectTests.swift
+//  RecipeMaster_ProjectTests
+//
+//  Created by Athlosn90 on 22/01/2021.
+//  Copyright © 2021 AndreaSanna. All rights reserved.
+//
+
+import XCTest
+@testable import RecipeMaster_Project
+
+class RecipeMaster_ProjectTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
